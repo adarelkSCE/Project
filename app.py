e3a170d9c8aeda80189b796dd1821d2a7cde45f0
adarelk2 <adarelk2@gmail.com>
2025-11-12 00:53:39 +0200

    init mvc base

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ from flask import render_template
 app = Flask(__name__)
 @app.route("/<controller_name>")
 def test(controller_name):
-    print(controller_name)
-    print(request.args['method'])
-    return render_template("pages/index.html", user={"name": "Adar"}, lang="he", dir="rtl", script=request.args['method'])
+    return render_template("pages/index.html", user={"name": request.args['name']}, page=controller_name)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000)
