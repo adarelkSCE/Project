93835776cc0ebc06d520b2315223afc28a7dff16
adarelk2 <adarelk2@gmail.com>
2026-01-04 20:45:10 +0200

    injection container to every controller

--- controllers/building_details_controller.py
+++ controllers/building_details_controller.py
@@ -1,11 +1,11 @@
 from core.controller_base import ControllerBase
-from container import createModel
-from container import createService
 
 class Building_detailsController(ControllerBase):
+    def __init__(self, _container):
+        self.building_service = _container.building_service
+        
     def print(self, params):
         id = params["id"]
-        service = createService("BuildingService")
-        building_by_service = service.get_buildings_with_rooms([id])[0]
+        building_by_service = self.building_service.get_buildings_with_rooms([id])[0]
         context = {"building": building_by_service}
         return self.responseHTML(context, "building-details")
